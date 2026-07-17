523d6ffe1cc5aa19c70afcf0d08c3259cf22468f
stefanench <stefanen@proton.me>
2026-07-17 22:29:18 +0300

    Create PdfDocument.java

---
+++ PdfDocument.java
@@ -0,0 +1,30 @@
+public class PdfDocument {
+
+    private final String name;
+
+    private final String text;
+
+    public PdfDocument(
+            String name,
+            String text
+    ) {
+
+        this.name = name;
+
+        this.text = text;
+
+    }
+
+    public String getName() {
+
+        return name;
+
+    }
+
+    public String getText() {
+
+        return text;
+
+    }
+
+}
