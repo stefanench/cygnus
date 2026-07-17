cfe64530cfe714d1d80e19de15beca1d71fa1c8f
stefanench <stefanen@proton.me>
2026-07-17 22:31:03 +0300

    Create SearchResult.java

---
+++ SearchResult.java
@@ -0,0 +1,19 @@
+public class SearchResult {
+
+    private final String file;
+
+    public SearchResult(
+            String file
+    ) {
+
+        this.file = file;
+
+    }
+
+    public String getFile() {
+
+        return file;
+
+    }
+
+}
